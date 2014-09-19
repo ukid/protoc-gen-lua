a688ae35d953806eff2ee8eba04893d7bd7a9da2
ukid <ukid@users.noreply.github.com>
2014-09-19 16:42:35 +0800

    fix io.format nil index  exception fix index nil.RegisterExtension err

--- protobuf/protobuf.lua
+++ protobuf/protobuf.lua
@@ -756,13 +756,13 @@ local function _AddIsInitializedMethod(message_descriptor, message_meta)
         for field, value in message_meta._member.ListFields(self) do
             if field.cpp_type == FieldDescriptor.CPPTYPE_MESSAGE then
                 if field.is_extension then
-                    name = io:format("(%s)", field.full_name)
+                    name = string.format("(%s)", field.full_name)
                 else
                     name = field.name
                 end
                 if field.label == FieldDescriptor.LABEL_REPEATED then
                     for i, element in ipairs(value) do
-                        prefix = io:format("%s[%d].", name, i)
+                        prefix = string.format("%s[%d].", name, i)
                         sub_errors = element:FindInitializationErrors()
                         for _, e in ipairs(sub_errors) do
                             errors[#errors + 1] = prefix .. e
